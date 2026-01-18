87e951d9f1a1d7f0f33ae6eeac459fdb7f774244
sudo-Arjun27 <sharma.arjun.0127@gmail.com>
2026-01-18 23:24:06 +0530

    Intro to Java
    First ever program in java written by me

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        /*
+ this is a comment in java, this is my first java program
+        this is a multiline comment
+
+        System.out.print("Hello!, world \n");
+        System.out.print("world. \n");
+        System.out.print("hmm mm \n");
+*/
+    int x=12;
+        double val = 69.96;
+        char grade='a';
+        boolean isForSale =true;
+
+        System.out.println(x + "\n");
+        System.out.println(val + "\n");
+        System.out.println(grade);
+        System.out.println(isForSale);
+
+    }
+
+}
